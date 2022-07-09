68eb7fc750b79774302fe994633b019d3b21e0d6
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-10 00:26:40 +0900

    Custom Exceptions for Unique Project Identifiers

--- src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.agileintelligence.ppmtool.services;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,12 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project) {
 
-        // Logic
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists ");
+        }
 
-        return projectRepository.save(project);
     }
 }
