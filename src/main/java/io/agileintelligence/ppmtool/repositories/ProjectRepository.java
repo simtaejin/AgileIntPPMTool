f77a61d03c6603ebe0dc4a7906b3def780db394e
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-10 00:45:18 +0900

    Find Project by Identifier

--- src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
+++ src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
-    @Override
-    Iterable<Project> findAllById(Iterable<Long> iterable);
+    Project findByProjectIdentifier(String projectId);
 
 }
--- src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -22,4 +22,16 @@ public class ProjectService {
         }
 
     }
+
+    public Project findByProjectIdentifier(String projectId) {
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project ID ' " + projectId + "' does not exist");
+        }
+
+        return project;
+    }
+
 }
