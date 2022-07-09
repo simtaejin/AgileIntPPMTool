bee455cb784eac51d234e084eedc02b802f893c2
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-09 10:31:00 +0900

    Project Object & Project Repository

---
+++ src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+package io.agileintelligence.ppmtool.repositories;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+
+}
