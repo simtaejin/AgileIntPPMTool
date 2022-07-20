a26337d67eb46e749c147daf45485eee32e8810c
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-21 02:03:02 +0900

    Backlog - ProjectTask relationship

--- src/main/java/io/agileintelligence/ppmtool/domain/Backlog.java
+++ src/main/java/io/agileintelligence/ppmtool/domain/Backlog.java
@@ -4,6 +4,8 @@ package io.agileintelligence.ppmtool.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Backlog {
@@ -18,6 +20,17 @@ public class Backlog {
     @JoinColumn(name="project_id", nullable = false)
     @JsonIgnore
     private Project project;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "backlog")
+    private List<ProjectTask> projectTasks = new ArrayList<>();
+
+    public List<ProjectTask> getProjectTasks() {
+        return projectTasks;
+    }
+
+    public void setProjectTasks(List<ProjectTask> projectTasks) {
+        this.projectTasks = projectTasks;
+    }
 
     public Project getProject() {
         return project;
--- src/main/java/io/agileintelligence/ppmtool/domain/ProjectTask.java
+++ src/main/java/io/agileintelligence/ppmtool/domain/ProjectTask.java
@@ -1,5 +1,7 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
@@ -18,6 +20,19 @@ public class ProjectTask {
     private String status;
     private Integer priority;
     private Date dueDate;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "backlog_id", updatable = false, nullable = false)
+    @JsonIgnore
+    private Backlog backlog;
+
+    public Backlog getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(Backlog backlog) {
+        this.backlog = backlog;
+    }
 
     @Column(updatable = false)
     private String projectIdentifer;
