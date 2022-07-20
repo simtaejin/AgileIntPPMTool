96054f6eae55d4abf87a2daafe290e3df4db9885
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-21 02:54:52 +0900

    Persist Project Task (Bug fix pending: setPriority)

--- src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package io.agileintelligence.ppmtool.services;
 
+import io.agileintelligence.ppmtool.domain.Backlog;
 import io.agileintelligence.ppmtool.domain.ProjectTask;
 import io.agileintelligence.ppmtool.repositories.BacklogRepository;
 import io.agileintelligence.ppmtool.repositories.ProjectTaskRepository;
@@ -15,8 +16,25 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
-    public ProjectTask addProjectTask() {
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
 
+        Integer BacklogSequence = backlog.getPTSequence();
+        BacklogSequence++;
+
+        projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
+        projectTask.setProjectIdentifer(projectIdentifier);
+
+//        if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
+//            projectTask.setPriority(3);
+//        }
+
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
+        }
+
+        return  projectTaskRepository.save(projectTask);
     }
 
 }
