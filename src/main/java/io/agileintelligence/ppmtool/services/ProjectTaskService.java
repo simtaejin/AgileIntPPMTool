511d8cac1520c199ee4340097f200316f36d165e
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-21 02:09:22 +0900

    Backlog - ProjectTask relationship

---
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,22 @@
+package io.agileintelligence.ppmtool.services;
+
+import io.agileintelligence.ppmtool.domain.ProjectTask;
+import io.agileintelligence.ppmtool.repositories.BacklogRepository;
+import io.agileintelligence.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask() {
+
+    }
+
+}
