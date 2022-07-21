1d00175fb4420e8b1c27126dc2f9c42b887b3970
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-21 11:30:22 +0900

    Find ProjectTask by projectSequence w/Validation

--- src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -73,7 +73,21 @@ public class ProjectTaskService {
     }
 
     public ProjectTask findByProjectSequence(String backlog_id, String pt_id) {
+        Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
 
-        return projectTaskRepository.findByProjectSequence(pt_id);
+        if(backlog==null){
+            throw new ProjectNotFoundException("Project with ID: '"+backlog_id+"' does not exist");
+        }
+
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+        if (projectTask==null) {
+            throw new ProjectNotFoundException("Project Task '"+backlog_id+"' not found");
+        }
+
+        if (!projectTask.getProjectIdentifier().equals(backlog_id)) {
+            throw new ProjectNotFoundException("Project Task '"+pt_id+"' does not exist in project: '" +backlog_id);
+        }
+
+        return projectTask;
     }
 }
