d5109dd48366bceda27c08247d909ea8715a5a4b
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-21 12:20:03 +0900

    Finish up with update validation and delete

--- src/main/java/io/agileintelligence/ppmtool/web/BacklogController.java
+++ src/main/java/io/agileintelligence/ppmtool/web/BacklogController.java
@@ -57,8 +57,13 @@ public class BacklogController {
 
         ProjectTask updatedTask = projectTaskService.updateByProjectSequence(projectTask, backlog_id, pt_id);
 
-
         return new ResponseEntity<ProjectTask>(updatedTask, HttpStatus.OK);
     }
 
+    @DeleteMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> deleteProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id) {
+        projectTaskService.deletePTByProjectSequence(backlog_id, pt_id);
+
+        return new ResponseEntity<String>("Project Task "+pt_id+" was deleted successfully", HttpStatus.OK);
+    }
 }
