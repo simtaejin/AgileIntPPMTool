463da6515473da32c7e18b1e5e5043e1f3c74497
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-07-28 13:30:03 +0900

    User registration part 3

--- src/main/java/io/agileintelligence/ppmtool/services/UserService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/UserService.java
@@ -20,6 +20,7 @@ public class UserService {
 
         try {
             newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            newUser.setConfirmPassword("");
             return userRepository.save(newUser);
 
         } catch (Exception e) {
